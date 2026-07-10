bcb8624be6f21197bbb571a8999d7fe83212db1a
rajeshdrajesh309 <rajeshdrajesh309@gmail.com>
2026-07-10 14:34:57 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,81 @@
+#############################
+# AWS Configuration
+#############################
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment Environment"
+  type        = string
+}
+
+#############################
+# Networking
+#############################
+
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "Public Subnet CIDR"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "Private Subnet CIDR"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability Zone"
+  type        = string
+}
+
+#############################
+# EC2
+#############################
+
+variable "ami_id" {
+  description = "Amazon Linux 2023 AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Existing EC2 Key Pair"
+  type        = string
+}
+
+variable "admin_ip" {
+  description = "Administrator Public IP"
+  type        = string
+}
+
+#############################
+# Database
+#############################
+
+variable "db_name" {
+  description = "Database Name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Database Username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database Password"
+  type        = string
+  sensitive   = true
+}
